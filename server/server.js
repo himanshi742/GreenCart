525e97b7cbdd1d9d3517f27db785b020ddde790c
Himanshi Bisht <himanshib328@gmail.com>
2026-01-28 14:34:25 +0530

    fix: remove trailing slash from allowed CORS origin URL

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://green-cart-frontend-phi.vercel.app/'],
+  origin: ['http://localhost:5173', 'https://green-cart-frontend-phi.vercel.app'],
   credentials: true
 }));
 
